b0fe49fa4a4bb1f633d3e2de3e1405442f9a407a
stellaGDS <57530628+stellaGDS@users.noreply.github.com>
2019-11-29 18:13:26 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-provider "cmdb" {
+provider "gds" {
   endpoint = "http://dev.gds.cloud/api/cmdb"
   version  = "0.0.1"
 }
 
-data "cmdb_config_item" "ci1" {
+data "gds_cmdb_config_item" "ci1" {
   cmdb_id = "alicloud_ess_scaling_rule_FNQXjYkH"
 }
 
 output "ci1_project_id" {
-  value = data.cmdb_config_item.ci1.project_id
+  value = data.gds_cmdb_config_item.ci1.project_id
 }
 output "ci1_supplier" {
-  value = data.cmdb_config_item.ci1.supplier
+  value = data.gds_cmdb_config_item.ci1.supplier
 }
