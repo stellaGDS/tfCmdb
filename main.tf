2d2dc8c8de0c7382e91725803fc9d32a61dd01d7
stellaGDS <57530628+stellaGDS@users.noreply.github.com>
2019-11-29 17:05:00 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "cmdb" {
+  endpoint = "http://dev.gds.cloud/api/cmdb"
+}
+
+data "cmdb_config_item" "ci1" {
+  cmdb_id = "alicloud_ess_scaling_rule_FNQXjYkH"
+}
+
+output "ci1_project_id" {
+  value = data.cmdb_config_item.ci1.project_id
+}
+output "ci1_supplier" {
+  value = data.cmdb_config_item.ci1.supplier
+}
